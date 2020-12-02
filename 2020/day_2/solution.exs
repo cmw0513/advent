76c3cef8298732131817c931b82ef00b9df91287
Christina Wolfe <christina.wolfe@ibanity.com>
2020-12-02 09:15:25 +0100

    Refactor to extract duplication

--- 2020/day_2/solution.exs
+++ 2020/day_2/solution.exs
@@ -1,34 +1,29 @@
-"puzzle.txt"
+parsed_password_lines =
+  "puzzle.txt"
   |> File.read!()
   |> String.split("\n", trim: true)
-  |> Enum.count(fn password_line ->
-    [letter_count_min, letter_count_max, letter, password] =
-      password_line
-      |> String.replace(":", "")
-      |> String.replace("-", " ")
-      |> String.split(" ")
-
-    range = Range.new(String.to_integer(letter_count_min), String.to_integer(letter_count_max))
-
-    letter_count =
-      password
-      |> String.graphemes()
-      |> Enum.count(&(&1 == letter))
-
-    Enum.member?(range, letter_count)
-  end)
-  |> IO.puts()
-
-"puzzle.txt"
-|> File.read!()
-|> String.split("\n", trim: true)
-|> Enum.count(fn password_line ->
-  [first_index, second_index, letter, password] =
+  |> Enum.map(fn password_line ->
     password_line
     |> String.replace(":", "")
     |> String.replace("-", " ")
     |> String.split(" ")
+  end)
 
+parsed_password_lines
+|> Enum.count(fn [letter_count_min, letter_count_max, letter, password] ->
+  range = Range.new(String.to_integer(letter_count_min), String.to_integer(letter_count_max))
+
+  letter_count =
+    password
+    |> String.graphemes()
+    |> Enum.count(&(&1 == letter))
+
+  Enum.member?(range, letter_count)
+end)
+|> IO.puts()
+
+parsed_password_lines
+|> Enum.count(fn [first_index, second_index, letter, password] ->
   letters = String.graphemes(password)
   letter_in_first_index = Enum.at(letters, String.to_integer(first_index) - 1)
   letter_in_second_index = Enum.at(letters, String.to_integer(second_index) - 1)
