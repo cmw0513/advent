4dd5ccfdbeb4c4efe6c1c1a83cb64ee7be0a56f9
Christina Wolfe <christina.wolfe@ibanity.com>
2020-12-06 10:51:11 +0100

    Day 5 part 2

--- 2020/day_5/solution.exs
+++ 2020/day_5/solution.exs
@@ -1,26 +1,34 @@
 defmodule BoardingPass do
-  def calculate_seat_id({row, seat}) do
-    calculate_row(row) * 8 + calculate_seat(seat)
-  end
-
-  def calculate_row(row) do
-    String.graphemes(row)
-    |> Enum.reduce(Enum.to_list(0..127), fn letter, range ->
-      {front_half, back_half} = Enum.split(range, round(Enum.count(range) / 2))
-      if letter == "B",
-       do: back_half,
-       else: front_half
+  def order_by_seat_id(boarding_passes) do
+    Enum.sort(boarding_passes, fn {row1, seat1}, {row2, seat2} ->
+      if row1 == row2,
+        do: seat1 >= seat2,
+        else: row1 <= row2
     end)
-    |> List.first()
   end
 
-  def calculate_seat(seat) do
-    String.graphemes(seat)
-    |> Enum.reduce(Enum.to_list(0..7), fn letter, range ->
-      {front_half, back_half} = Enum.split(range, round(Enum.count(range) / 2))
-      if letter == "R",
-       do: back_half,
-       else: front_half
+  def seat_id({row, seat}), do: row_number(row) * 8 + seat_number(seat)
+
+  def find_missing_seat_id({row, seats}) do
+    [seat_id] = all_seat_ids_in_row(row) -- Enum.map(seats, &seat_id/1)
+    seat_id
+  end
+
+  defp all_seat_ids_in_row(row) do
+    row_value = row_number(row) * 8
+    Enum.to_list(row_value..(row_value + 7))
+  end
+
+  defp row_number(row), do: calculate_number(row, 0..127, "B")
+
+  defp seat_number(seat), do: calculate_number(seat, 0..7, "R")
+
+  defp calculate_number(binary_partition_string, range, upper_half_letter) do
+    binary_partition_string
+    |> String.graphemes()
+    |> Enum.reduce(Enum.to_list(range), fn letter, possible_numbers ->
+      {lower_half, upper_half} = Enum.split(possible_numbers, round(Enum.count(possible_numbers) / 2))
+      if letter == upper_half_letter, do: upper_half, else: lower_half
     end)
     |> List.first()
   end
@@ -30,14 +38,17 @@ boarding_passes =
   "puzzle.txt"
   |> File.read!()
   |> String.split("\n", trim: true)
+  |> Enum.map(&String.split_at(&1, 7))
 
 boarding_passes
-|> Enum.map(&String.split_at(&1, 7))
-|> Enum.sort(fn {row1, seat1}, {row2, seat2} ->
-  if row1 == row2,
-    do: seat1 >= seat2,
-    else: row1 <= row2
-end)
+|> BoardingPass.order_by_seat_id()
 |> List.first()
-|> BoardingPass.calculate_seat_id()
+|> BoardingPass.seat_id()
+|> IO.inspect()
+
+boarding_passes
+|> Enum.group_by(fn {row, _seat} -> row end)
+|> Enum.filter(fn {_row, seats} -> Enum.count(seats) != 8 end)
+|> Enum.at(1)
+|> BoardingPass.find_missing_seat_id()
 |> IO.inspect()
