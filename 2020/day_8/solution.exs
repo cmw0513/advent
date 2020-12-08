44ddd022fad2077d0c2e1cd03acac7c87009f9a7
Christina Wolfe <christina.wolfe@ibanity.com>
2020-12-08 10:07:58 +0100

    Part 2

--- 2020/day_8/solution.exs
+++ 2020/day_8/solution.exs
@@ -1,14 +1,14 @@
 defmodule BootCode do
-  def run_instructions(instructions, index, acc, past_instruction_indexes) do
+  def run_instructions(instructions, index \\ 0, acc \\ 0, past_instruction_indexes \\ []) do
     if index in past_instruction_indexes do
-      acc
+      %{acc: acc, index: index, past_instruction_indexes: past_instruction_indexes}
     else
-      instruction = Enum.at(instructions, index)
       past_instruction_indexes = [index | past_instruction_indexes]
-      case instruction do
+      case Enum.at(instructions, index) do
         {"nop", _} -> run_instructions(instructions, index + 1, acc, past_instruction_indexes)
         {"acc", argument} -> run_instructions(instructions, index + 1, acc + argument, past_instruction_indexes)
         {"jmp", argument} -> run_instructions(instructions, index + argument, acc, past_instruction_indexes)
+        _ -> acc
       end
     end
   end
@@ -21,5 +21,16 @@ instructions =
   |> Enum.map(&String.split/1)
   |> Enum.map(fn [operation, argument] -> {operation, String.to_integer(argument)} end)
 
-BootCode.run_instructions(instructions, 0, 0, [])
-|> IO.inspect
+%{acc: acc, index: index, past_instruction_indexes: past_instruction_indexes} = BootCode.run_instructions(instructions) |> IO.inspect
+
+past_instruction_indexes
+|> Enum.filter(fn x -> Enum.at(instructions, x) |> elem(0) == "jmp" end)
+|> Enum.find_value(fn replacement_index ->
+  argument = instructions |> Enum.at(replacement_index) |> elem(1)
+  acc =
+    instructions
+    |> List.replace_at(replacement_index, {"nop", argument})
+    |> BootCode.run_instructions()
+  unless is_map(acc), do: acc
+end)
+|> IO.inspect()
