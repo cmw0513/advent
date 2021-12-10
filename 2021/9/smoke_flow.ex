e458cc7c7a2988785c490e6b07b78a3d5120834b
Christina Wolfe <cmw0513@gmail.com>
2021-12-10 21:47:47 +0100

    Finish Day 9

--- 2021/9/smoke_flow.ex
+++ 2021/9/smoke_flow.ex
@@ -1,47 +1,20 @@
-rows =
-  "example.txt"
-  # "input.txt"
-  |> File.read!()
-  |> String.split("\n", trim: true)
-  |> Enum.with_index()
-
-
-coordinates =
-  rows
-  |> Enum.reduce(%{}, fn {row, x}, acc ->
-    row
-    |> String.graphemes()
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.with_index()
-    |> Enum.map(fn {digit, y} ->
-      {{x, y}, digit}
-    end)
-    |> Enum.into(acc)
-  end)
-
-adjacent_values = fn {x, y} ->
-  [
-    coordinates[{x - 1, y}],
-    coordinates[{x + 1, y}],
-    coordinates[{x, y + 1}],
-    coordinates[{x, y - 1}]
-  ]
-end
-
-coordinates
-|> Enum.filter(fn {{x, y}, value} ->
-  adjacent_values = adjacent_values.({x, y})
-  value not in adjacent_values && Enum.min([value | adjacent_values]) == value
-end)
-|> Enum.map(fn {_, value} -> value + 1 end)
-|> Enum.sum()
-|> IO.inspect(label: "Part 1")
-
 defmodule SmokeFlow do
-  def adjacent_values(coordinates, point) do
-    point
-    |> adjacent_coordinates()
-    |> Enum.map(fn point -> coordinates[point] end)
+  def low_points(coordinate_heights) do
+    Enum.filter(coordinate_heights, fn {point, height} ->
+      point
+      |> adjacent_heights(coordinate_heights)
+      |> Enum.all?(&(&1 > height))
+    end)
+  end
+
+  def find_basin(point, coordinate_heights, basin \\ []) do
+    if point in basin or coordinate_heights[point] in [nil, 9] do
+      basin
+    else
+      point
+      |> adjacent_coordinates()
+      |> Enum.reduce([point | basin], &find_basin(&1, coordinate_heights, &2))
+    end
   end
 
   def adjacent_coordinates({x, y}) do
@@ -53,21 +26,42 @@ defmodule SmokeFlow do
     ]
   end
 
-  def adjacent_basin_coordinates(coordinates, point) do
+  def adjacent_heights(point, coordinates) do
     point
     |> adjacent_coordinates()
-    |> Enum.reject(fn {x, y} -> coordinates[{x, y}] in [nil, 9] end)
-  end
-
-  def basin_coordinates(coordinates, point) do
-    adjacent_basin_coordinates(coordinates, point)
-    |> Enum.map_reduce([])
+    |> Enum.map(&(coordinates[&1]))
   end
 end
 
-# Enum.reduce(coordinates, [], fn {point, _value}, acc ->
-#   if point in List.flatten(acc),
-#     do: acc,
-#     else: [SmokeFlow.adjacent_basin_coordinates(coordinates, point) | acc]
-# end)
-# |> IO.inspect
+coordinate_heights =
+  # "example.txt"
+  "input.txt"
+  |> File.read!()
+  |> String.split("\n", trim: true)
+  |> Enum.with_index()
+  |> Enum.reduce(%{}, fn {row, x}, acc ->
+    row
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.with_index()
+    |> Enum.map(fn {height, y} -> {{x, y}, height} end)
+    |> Enum.into(acc)
+  end)
+
+coordinate_heights
+|> SmokeFlow.low_points()
+|> Enum.map(fn {_, value} -> value + 1 end)
+|> Enum.sum()
+|> IO.inspect(label: "Part 1")
+
+coordinate_heights
+|> SmokeFlow.low_points()
+|> Enum.map(fn {point, _} ->
+  point
+  |> SmokeFlow.find_basin(coordinate_heights, [])
+  |> length()
+end)
+|> Enum.sort(:desc)
+|> Enum.take(3)
+|> Enum.product()
+|> IO.inspect(label: "Part 2")
